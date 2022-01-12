0e4ed0df446e12c883f1531d7e6488ea9b038b2f
dltkdgksqhr <78789ks@naver.com>
2022-01-12 20:12:52 +0900

    youcanseeme

---
+++ src/JDBC/app/console/adas2.java
@@ -0,0 +1,11 @@
+package JDBC.app.console;
+
+public class adas2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("You Can See me?");
+
+	}
+
+}
